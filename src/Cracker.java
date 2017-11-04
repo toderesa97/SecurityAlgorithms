4ce7f9d662b5d920bed15ef195c1e979e61a32f7
test <toderesa97@gmail.com>
2017-11-04 18:20:53 +0100

    Added crackers of monoalphabetic ciphers (such as caesar) and affine cipher. Expected frequency analysis.

--- src/Cracker.java
+++ src/Cracker.java
@@ -1,12 +1,12 @@
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Objects;
 import javax.xml.bind.DatatypeConverter;
 
 public class Cracker {
+
+    private String letters;
 
     public static String crackSha256(String h, String chars) {
         // if the string is lower than 10 chars, it generates 9!=362880 possibilities at most.
@@ -58,7 +58,8 @@ public class Cracker {
         }
         int remainder = -1;
         while (remainder != 0) {
-            greater = lower*(greater/lower) + (remainder=greater-(lower*(greater/lower)));
+            int factor = greater/lower;
+            int dummy = lower*factor + (remainder=greater-(lower*factor));
             greater = lower;
             lower = remainder;
         }
@@ -72,12 +73,12 @@ public class Cracker {
             return variations;
         }
         for (int i = 0; i < chars.length(); i++) {
-            String c = "";
+            StringBuilder c = new StringBuilder();
             for (int j = 0; j < chars.length(); j++) {
                 if (i == j) continue;
-                c += chars.charAt(j);
+                c.append(chars.charAt(j));
             }
-            ArrayList<String> pet = permute(c);
+            ArrayList<String> pet = permute(c.toString());
             for (String s : pet) {
                 variations.add(chars.charAt(i)+s);
             }
@@ -85,7 +86,7 @@ public class Cracker {
         return variations;
     }
 
-    private static String generateSHA256 (String str) {
+    public static String generateSHA256 (String str) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(str.getBytes("UTF-8"));
@@ -95,4 +96,93 @@ public class Cracker {
             return null;
         }
     }
+
+    public static String encrypt_shift_cipher (String x, int key) {
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder y = new StringBuilder();
+        for (int i = 0; i < x.length(); i++) {
+            if (x.charAt(i) == ' ') {
+                y.append(" ");
+                continue;
+            }
+            y.append(letters.charAt(Math.floorMod(letters.indexOf(x.charAt(i)) + key, 26)));
+        }
+        return y.toString();
+    }
+
+    public static String encrypt_affine_cipher (String x, int a, int b) {
+        // to obtain the digest apply y=(a*x+b)mod(26)
+        // a must be relatively prime to 26, ie: gcd(a, 26)=1
+        if (euclidean_gcd(26, a) != 1) {
+            return null;
+        }
+        StringBuilder y = new StringBuilder();
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < x.length(); i++) {
+            if (x.charAt(i) == ' ') {
+                y.append(" ");
+                continue;
+            }
+            y.append(letters.charAt(Math.floorMod(letters.indexOf(x.charAt(i)) * a + b, 26)));
+        }
+        return y.toString();
+    }
+
+    public static List<String> decrypt_affine_cipher (String y) {
+        // reverting the modular equation y=(a*x+b)mod(26)
+        // (y-b)mod(26)=a*x*mod(26) -> ((y-b)*inv(a))mod(26)=x
+        int[] keys_a = {1,3,5,7,9,11,15,17,19,21,23,25};
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        String[] common_words = {"the", "of", "to", "is", "are", "and", "on", "from"};
+        StringBuilder x;
+        ArrayList<String> posible = new ArrayList<>();
+        for (int a = 0; a < keys_a.length; a++) {
+            for (int b = 0; b < 26; b++) {
+                x = new StringBuilder();
+                for (int i = 0; i < y.length(); i++) {
+                    if (y.charAt(i) == ' ') {
+                        x.append(" ");
+                        continue;
+                    }
+                    x.append(letters.charAt(Math.floorMod(calculateInverse(keys_a[a],26)*(letters.indexOf(y.charAt(i))-b),26)));
+                }
+                for (String w : common_words) {
+                    if (x.toString().matches(".*\\b"+w+"\\b.*")) {
+                        posible.add(x.toString());
+                    }
+                }
+            }
+        }
+        return posible;
+    }
+
+    public static List<String> decrypt_shift_cipher (String y) {
+        // based on shift encryption
+        String[] common_words = {"the", "are","of", "to","on" ,"is", "and", "from"};
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        // x=d(y)=(y-k)mod(26)
+        StringBuilder x;
+        ArrayList<String> possible = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            x = new StringBuilder();
+            for (int j = 0; j < y.length(); j++) {
+                if (y.charAt(j) == ' ') {
+                    x.append(" ");
+                    continue;
+                }
+                x.append(letters.charAt(Math.floorMod(letters.indexOf(y.charAt(j)) - i, 26)));
+            }
+            for (String w : common_words) {
+                if (! x.toString().contains(" ")) {
+                    if (x.toString().contains(w)) possible.add(x.toString());
+                    continue;
+                }
+                if (x.toString().matches(".*\\b"+w+"\\b.*")) {
+                    possible.add(x.toString());
+                }
+            }
+        }
+        return possible;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-/**
- * Created by test on 03/08/2017.
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Cracker.euclidean_gcd(45, 7));
+        // returns a possible set of understandable sentences
+        System.out.println(Cracker.decrypt_affine_cipher(Cracker.encrypt_affine_cipher("hi how are u",7,2)));
+        String encrypted_mesg = Cracker.encrypt_affine_cipher("life is too hard",21,124);
+        System.out.println(encrypted_mesg);
+        System.out.println(Cracker.decrypt_affine_cipher(encrypted_mesg));
+
+        encrypted_mesg = Cracker.encrypt_shift_cipher("this is an encrypted message using caesar cipher",21);
+        System.out.println(encrypted_mesg);
+        System.out.println(Cracker.decrypt_shift_cipher(encrypted_mesg));
+
+        // who wrote the message ?
+        encrypted_mesg = "xultpaajcxitltlxaarpjhtiwtgxktghidhipxciwtvgtpilpit" +
+                "ghlxiwiwtxgqadds";
+        System.out.println(Cracker.decrypt_shift_cipher(encrypted_mesg));
+        System.out.println(Cracker.
+                encrypt_shift_cipher("if we all unite we will cause the rivers " +
+                        "to stain the great waters with their blood",15).replaceAll(" ", ""));
+        // Tecumseh
     }
 }
